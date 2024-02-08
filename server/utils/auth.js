9ace9e9817e5027adbb61e3f2eb4d95415e56c8c
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-08 04:29:46 -0500

    Implementation of graphQL server middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,41 @@
-const jwt = require('jsonwebtoken');
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const path = require('path');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+const startApolloServer = async () => {
+  await server.start();
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
+  app.use('/graphql', server.getMiddleware());
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
 };
+
+startApolloServer();
+
