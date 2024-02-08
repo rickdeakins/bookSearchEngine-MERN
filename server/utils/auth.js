9a8b120a6325ae1173779dfe2deb397e11c64c6e
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-08 04:35:52 -0500

    Middleware revised to include Apollo/GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,41 +1,17 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const path = require('path');
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-const startApolloServer = async () => {
-  await server.start();
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  app.use('/graphql', server.getMiddleware());
-
-  // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
-  });
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
-
-startApolloServer();
-
